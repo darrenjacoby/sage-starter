588378e29e7b297230e403692106b7c1076f0591
Darren Jacoby <darren@jacoby.dev>
2020-04-24 18:28:54 +0200

    sage-starter

--- tailwind.js
+++ tailwind.js
@@ -1,12 +1,26 @@
 module.exports = {
   theme: {
     screens: {
-      sm: '36rem',
-      md: '48rem',
-      lg: '62rem',
-      xl: '80rem',
-      xx: '100rem',
+      'sm': '36rem',
+      'md': '48rem',
+      'lg': '62rem',
+      'xl': '80rem',
+      '2xl': '100rem',
     },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '6rem',
+      '8xl': '8rem',
+    }
   },
   variants: {},
   plugins: [],
